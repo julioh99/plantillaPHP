bc5551908d3bb3956863190705a888e0da670a6d
Juan Alejandro Gonzalez Castrillón <57808104+juan123castrillon@users.noreply.github.com>
2020-11-14 16:53:58 -0500

    Se trato de organizar la insercción de registro de personas pero sale error 500 de servidor

--- funciones/registro.funciones.php
+++ funciones/registro.funciones.php
@@ -14,9 +14,8 @@ if (isset($_POST['BtnRegistro'])) {
     if ($_POST['TxtId']!=null ||$_POST['TxtNombres']!=null ||$_POST['TxtApellidos']!=null ||$_POST['CbxSexo']!=0||
         $_POST['TxtTelefono']!=null||$_POST['TxtCorreo']!=null||$_POST['TxtDireccion']!=null) {
 
-        $strSQL = " INSERT INTO tblpersonas (TipoId,PKIdentificacion,Nombres,Apellidos,FKCodigo_TblSexo,Correo,Telefono,Direccion) 
+        $strSQL = "INSERT INTO tblpersonas (TipoId,PKIdentificacion,Nombres,Apellidos,FKCodigo_TblSexo,Correo,Telefono,Direccion) 
                     VALUES (:TId ,:ID, :Nombres, :Apellidos, :Sexo, :Telefono,:Correo,:Direccion);";
-                    
 
         $aryParametros = [
             ':TId' => $TipoId,
@@ -26,19 +25,14 @@ if (isset($_POST['BtnRegistro'])) {
             ':Sexo' => $sexo,
             ':Telefono' => $telefono,
             ':Correo' => strtoupper($correo),
-            ':Direccion' => strtoupper($direccion)          
+            ':Direccion' => strtoupper($direccion)
         ]; // Arreglo asociativo => :propiedad
-
 
         $objSentencia = $pdo->prepare($strSQL);
         # $objSentencia->execute(); // Ejecutar sin parametros
         $objSentencia->execute($aryParametros);
-
-       
-
-
-
         echo "<script>alert('Registrado Correctamente ✔')</script>";
+        echo"<script>alertify.success('Guardado Correctamente');</script>";
         header("LOCATION : ../pages/login.php");
     }
 }
